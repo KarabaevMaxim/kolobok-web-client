aa394d3abb6c1d09bb88d94cf309d50b08e72098
Karabaev_MO <karabaev_mo@directum.ru>
2020-10-26 16:02:59 +0400

    Написал метод генерации ури

---
+++ KolobokClient/Params.cs
@@ -0,0 +1,13 @@
+namespace KolobokClient
+{
+  public static class Params
+  {
+    public static bool UseHttps => true;
+      
+    public static string Host => "localhost";
+
+    public static int Port => 4000;
+    
+    public static string ApiPath => "api/v1";
+  }
+}
--- KolobokClient/WebClient.cs
+++ KolobokClient/WebClient.cs
@@ -1,7 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace KolobokClient
 {
   public class WebClient
   {
-    
+    public Uri ConstructUri(bool useHttps, string host, int port, string apiPath, string controller, string method, Dictionary<string, string> query = null)
+    {
+      var uriBuilder = new UriBuilder();
+      uriBuilder.Scheme = useHttps ? "https" : "http";
+      uriBuilder.Host = host;
+      uriBuilder.Port = port;
+      uriBuilder.Path = $"{apiPath}/{controller}/{method}";
+      if (query != null)
+      {
+        var sb = new StringBuilder();
+        var i = 0;
+        
+        foreach (var param in query)
+        {
+          sb.Append($"{param.Key}={param.Value}");
+          
+          if (i < query.Count)
+            sb.Append("&");
+          
+          i++;
+        }
+
+        uriBuilder.Query = sb.ToString();
+      }
+      
+      return uriBuilder.Uri;
+    }
   }
 }
